f69b9594109a2dbdb5af5f0cc0ddcb06ddd2f484
aso-ryo <2301181@s.asojuku.ac.jp>
2024-10-17 01:31:03 +0000

    変更完了

---
+++ G01_login.php
@@ -0,0 +1,6 @@
+<h1>Stylista</h1>
+ログイン<br>
+<input type="text" name="mail" value="メールアドレス"><br>
+<input type="text" name="pass" value="パスワード"><br>
+<button type="submit" name="login">ログイン</button><br>
+<button type="subimt" name="touroku">新規会員登録はこちら</button>
