54186b5b76105b6bdaf4d24f09197eb728430833
aso-ryo <2301181@s.asojuku.ac.jp>
2024-10-17 10:19:51 +0900

    Add files via upload

---
+++ favorite.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お気に入り画面</title>
+</head>
+<body>
+Stylista
+    <input type="text" name="query" placeholder="アイテムの検索">
+    <button type="submit">検索</button>
+    <button type="submit" name="cato"></button>
+    <button type="submit" name="favorite"></button>
+    <button type="submit" name="mypage"></button>
+    <p>お気に入り</p>
+    
+
+
+    
+</body>
+</html>
