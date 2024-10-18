5d01b6fdd295a268078a9da10f088803ca0b0f46
aso-ryo <2301181@s.asojuku.ac.jp>
2024-10-18 11:58:12 +0900

    新規作成

---
+++ G25_goods_history.php
@@ -0,0 +1,6 @@
+Stylista
+    <input type="text" name="query" placeholder="アイテムの検索">
+    <button type="submit">検索</button>
+    <button type="submit" name="kato"></button>
+    <button type="submit" name="favorite"></button>
+    <button type="submit" name="mypage"></button>
