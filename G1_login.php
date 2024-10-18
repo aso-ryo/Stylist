f97aa7c0155d0e0c7f27e24fc3748f5c17333082
koudamiyuu <2301157@s.asojuku.ac.jp>
2024-10-18 11:40:04 +0900

    henkou

--- G1_login.php
+++ G1_login.php
@@ -1,7 +1,7 @@
 <h1>Stylista</h1>
 ログイン<br>
-<input type="text" name="mail" value="メールアドレス"><br>
-<input type="text" name="pass" value="パスワード"><br>
+<input type="text" name="mail" placeholder="メールアドレス"><br>
+<input type="text" name="pass" placeholder="パスワード"><br>
 <button type="submit" name="login">ログイン</button><br>
 <button type="subimt" name="touroku">新規会員登録はこちら</button>
 　
--- new_user.php
+++
@@ -1,5 +0,0 @@
-<h1>Stylist</h1>
-<h2>新規会員登録</h2>
-<input type="text" name="mail" placeholder="メールアドレス"><br>
-<input type="text" name="pass" placeholder="パスワード"><br>
-<button type="submit" name="action" value="send">個人情報の入力へ進む</button>
