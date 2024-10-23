a0a7a490f6a2f84f13fb69971515c7cd77ae3499
koudamiyuu <2301157@s.asojuku.ac.jp>
2024-10-23 10:32:34 +0900

    henkou

--- G16_favorite_delete.php
+++ G16_favorite_delete.php
@@ -12,7 +12,7 @@
     <button type="submit" name="kato"></button>
     <button type="submit" name="favorite"></button>
     <button type="submit" name="mypage"></button>
-    お気に入り
+    <p>お気に入り</p>
 
     お気に入りから削除しますか<br>
     はい　/　いいえ
--- G8_age.gender.php
+++ G8_age.gender.php
@@ -12,6 +12,27 @@ Stylista
     <button type="submit" name="kato"></button>
     <button type="submit" name="favorite"></button>
     <button type="submit" name="mypage"></button>
+    <p>絞り込み</p>
+        年齢
+        <select name="age">
+            <option value="10">10代</option>
+            <option value="20">20代</option>
+            <option value="30">30代</option>
+            <option value="40">40代</option>
+            <option value="50">50代</option>
+            <option value="60">60代</option>
+        </select>
+        年齢
+        <select name="gender">
+            <option value="10">男性</option>
+            <option value="20">女性</option>
+            <option value="30">ユニセックス</option>
+        </select>
+    <form action="" method="post">
+        <button type="submit">変更する</button>
+    </form>
 
+    <p>おすすめ商品</p>
+    
 </body>
 </html>
