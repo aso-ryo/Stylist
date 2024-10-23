5bc0928480d4420fe7e9b5961402437b1519e694
koudamiyuu <2301157@s.asojuku.ac.jp>
2024-10-23 10:49:02 +0900

    Update G14_serch_pop.php

--- G14_serch_pop.php
+++ G14_serch_pop.php
@@ -1,6 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 Stylista
     <input type="text" name="query" placeholder="アイテムの検索">
     <button type="submit">検索</button>
     <button type="submit" name="kato"></button>
     <button type="submit" name="favorite"></button>
     <button type="submit" name="mypage"></button>
+    <?php
+        echo $_POST['query'],'の検索結果';
+        ?>
+</body>
+</html>
