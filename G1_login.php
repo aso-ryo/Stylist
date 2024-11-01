7d38a78abe3697f14f67314b2b025a9b4da7334c
aso-ryo <2301181@s.asojuku.ac.jp>
2024-11-01 12:45:06 +0900

    更新完了

--- G1_login.php
+++ G1_login.php
@@ -1,33 +1,11 @@
 <h1>Stylista</h1>
-ログイン<br>
+<h2>ログイン</h2>
 
-<div className="screen">
-      <div className="div">
-        <div className="text-wrapper-2">ログイン</div>
-
-        <div className="overlap-group">
-          <div className="text-wrapper-3">メールアドレス</div>
-        </div>
-
-        <div className="overlap">
-          <div className="text-wrapper-3">パスワード</div>
-        </div>
-
-        <Button>
-          className="button-instance"
-          divClassName="design-component-instance-node"
-          label="ログイン"
-          size="medium"
-          variant="primary"
-</button>
-        <Button>
-          className="button-2"
-          divClassName="design-component-instance-node"
-          label="新規会員登録はこちら"
-          size="medium"
-          variant="primary"
-        </button>>
-        <div className="text-wrapper-4">Stylista</div>
-      </div>
-    </div>
-  
+<input type="text" name="mail" placeholder="メールアドレス"><br>
+<input type="text" name="pass" placeholder="パスワード"><br>
+<form action="G6_toppage.php" method="post">
+<button type="submit" name="login">ログイン</button><br>
+</form>
+<form action="" method="post">
+<button type="subimt" name="touroku">新規会員登録はこちら</button>
+</form>
