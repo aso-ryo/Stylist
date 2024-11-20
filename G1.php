3c3d0db0d965d1c0596929c278826d190dbec69c
aso-ryo <2301181@s.asojuku.ac.jp>
2024-11-20 09:57:56 +0900

    ｓｃｃ

--- G1.php
+++ G1.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="..Stylist/.vscode/CSS/G1/scc">
+    <link rel="stylesheet" href="C:\xampp\htdocs\Stylist\Stylist\.vscode\CSS\G1.css">
 </head>
 <body>
 
