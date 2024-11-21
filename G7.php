8a16612622333883817af9a6f765355f3bbb3025
koudamiyuu <2301157@s.asojuku.ac.jp>
2024-11-21 10:20:23 +0900

    w

--- G7.php
+++ G7.php
@@ -1,21 +1,34 @@
- <!DOCTYPE html>
- <html lang="en">
+Stylista
+    <input type="text" name="query" placeholder="アイテムの検索">
+    <button type="submit">検索</button>
+    <button type="submit" name="kato"></button>
+    <button type="submit" name="favorite"></button>
+    <button type="submit" name="mypage"></button>
+<?
+    $pdo=new PDO('mysql:host=mysql309.phy.lolipop.lan;
+                dbname=LAA1554862-kaihatsu;charset=utf8',
+                'LAA1554862',
+                'aso2024');
 
- <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href=".vscode/CSS/G7.css">
-     <title>Document</title>
- </head>
+        $sql = $pdo->prepare("select * from goods where goods_id=?");
+        $sql->execute([$_POST['id']]);
+        $reviews = $sql->fetchAll(PDO::FETCH_ASSOC);
+        if ($reviews) {
+            foreach ($reviews as $review) {
+                echo '<p><img src="' . $review['image'] . '"></p>';
+                echo '<p>' . $review['goods_name'] . '</p>';
+                echo '<p>' . $review['price'] . '</p>';
+                echo '<p>' . $review['explain'] . '</p>';
+                $s=$review['goods_id'];  
+            }
+        }
+        $sql = $pdo->prepare("select stock from stock where goods_id=?");
+        $sql->execute([$s]);
+        $stock = $sql->fetchColumn();
+        if ($stock !== false && $stock > 0) {
+            echo "在庫あり: $stock 個";
+        } else {
+            echo "在庫なし";
+        }
 
- <body>
-     Stylista
-     <input type="text" name="query" placeholder="アイテムの検索">
-     <button type="submit">検索</button>
-     <button type="submit" name="kato"></button>
-     <button type="submit" name="favorite"></button>
-     <button type="submit" name="mypage"></button>
-
- </body>
-
- </html>
+?>
