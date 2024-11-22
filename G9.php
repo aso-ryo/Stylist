ed1db0264c0fe406074c7f0385c35cd990584b9d
koudamiyuu <2301157@s.asojuku.ac.jp>
2024-11-22 10:59:38 +0900

    a

--- G9.php
+++ G9.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+    if(isset($_POST['user_name'])&&isset($_POST['user_name'])&&isset($_POST['user_name_kana'])&&isset($_POST['e-mail'])&&isset($_POST['password'])&&isset($_POST['birthday'])&&isset($_POST['adless_number'])&&isset($_POST['adless'])&&isset($_POST['tell'])&&isset($_POST['cart_id'])){
+        $_SESSION['user_id']=$_POST['cart_id'];
+        $_SESSION['name']=$_POST['name'];
+        $_SESSION['name_kana']=$_POST['name_kana'];
+        $_SESSION['mail']=$_POST['mail'];
+        $_SESSION['pass']=$_POST['pass'];
+        $_SESSION['birthday']=$_POST['birthday'];
+        $_SESSION['yuubin']=$_POST['yuubin'];
+        $_SESSION['juusyo']=$_POST['juusyo'];
+        $_SESSION['tell']=$_POST['tell'];
+        $_SESSION['cart_id']=$_POST['cart_id'];
+       }
+       ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
