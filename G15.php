82a6c9b09a1f0eed0f4b35798b3a9f77f11f0de5
2301145 <2301145@s.asojuku.ac.jp>
2024-11-27 16:01:34 +0900

    a

--- G15.php
+++ G15.php
@@ -3,20 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=".vscode/CSS/G17.css">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/G15.css">
+    <link rel="stylesheet" href="./css/header.css">
     <title>会員情報変更完了画面</title>
 </head>
 <body>
-Stylista
-    <input type="text" name="query" placeholder="アイテムの検索">
-    <button type="submit">検索</button>
-    <button type="submit" name="kato"></button>
-    <button type="submit" name="favorite"></button>
-    <button type="submit" name="mypage"></button>
-    <h1>会員情報の変更が完了しました</h1>
+<header>
+        <a href="" class="site__name">Stylista</a>
+        <form class="search__box" action="" method="post">
+            <input class="search__bar" type="text" name="query" placeholder="アイテムの検索">
+            <button type="submit"><i class="search__icon bi bi-search"></i>
+            </button>
+        </form>
+        <form action="" method="post">
+            <button type="submit"><i class="header__icon bi bi-cart"></i></button>
+        </form>
+        <form action="" method="post">
+            <button type="submit"><i class="header__icon bi bi-star"></i></button>
+        </form>
+        <form action="" method="post">
+            <button type="submit"><i class="header__icon bi bi-person"></i></button>
+        </form>
+    </header>
+ 
+    <p class="kanryou">会員情報の変更が完了しました</p><br>
+ 
     <form action="G19_user_info.php" method="post">
-        <button type="submit">会員情報画面へ戻る</button>
+        <button type="submit" class="button">会員情報画面へ戻る</button>
     </form>
 </body>
 </html>
-    
+ 
