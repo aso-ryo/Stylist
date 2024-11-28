e791113b25efb6f96b38814ffe8e6ebc60b552c6
aso-ryo <2301181@s.asojuku.ac.jp>
2024-11-28 15:02:52 +0900

    G4完成

--- G4.php
+++ G4.php
@@ -4,72 +4,81 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/G4.css">
     <link rel="stylesheet" href="./css/header.css">
-    <link rel="stylesheet" href="">
     <title>トップページ</title>
 </head>
+
 <body>
-<header>
-        <a href="G4.php" class="site__name">Stylista</a>
+    <header>
+        <a href="" class="site__name">Stylista</a>
         <form class="search__box" action="" method="post">
             <input class="search__bar" type="text" name="query" placeholder="アイテムの検索">
             <button type="submit"><i class="search__icon bi bi-search"></i>
             </button>
         </form>
-        <form action="G8.php" method="post">
+        <form action="" method="post">
             <button type="submit"><i class="header__icon bi bi-cart"></i></button>
         </form>
-        <form action="G12.php" method="post">
+        <form action="" method="post">
             <button type="submit"><i class="header__icon bi bi-star"></i></button>
         </form>
-        <form action="G13.php" method="post">
+        <form action="" method="post">
             <button type="submit"><i class="header__icon bi bi-person"></i></button>
         </form>
     </header>
 
-<category>
-    <p>カテゴリーから探す</p>
-    <a href="">トップス</a><br>
-    <a href="">アウター</a><br>
-    <a href="">パンツ</a><br>
-    <a href="">オールインワン</a><br>
-    <a href="">スカート</a><br>
-    <a href="">ワンピース</a><br>
-    <a href="">シューズ</a><br>
-    <a href="">バッグ</a><br>
-    <a href="">アクセサリー</a><br>
-    <a href="">帽子</a><br>
-    <a href="">ファッション雑貨</a><br>
-</category>
 
-<div id="category-cards">
-    <?php
-        $pdo=new PDO('mysql:host=mysql309.phy.lolipop.lan;
-        dbname=LAA1554862-kaihatsu;charset=utf8',
-        'LAA1554862',
-        'aso2024');
+    <div class="image-topp">
+        <img src="./images/トップページ画像１.png" alt="画像1">
+        <img src="./images/トップページ画像２.png" alt="画像2">
+    </div><br>
 
+    <section class="main__container">
+        <div class="category__container">
+            <h3>カテゴリーから探す</h3>
+            <a href="">トップス</a>
+            <a href="">アウター</a>
+            <a href="">パンツ</a>
+            <a href="">オールインワン</a>
+            <a href="">スカート</a>
+            <a href="">ワンピース</a>
+            <a href="">シューズ</a>
+            <a href="">バッグ</a>
+            <a href="">アクセサリー</a>
+            <a href="">帽子</a>
+            <a href="">ファッション雑貨</a>
+        </div>
+        <div class="card__container">
+            <?php
+            $pdo = new PDO(
+                'mysql:host=mysql309.phy.lolipop.lan;
+                    dbname=LAA1554862-kaihatsu;charset=utf8',
+                'LAA1554862',
+                'aso2024'
+            );
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql_reviews = "SELECT * FROM image_posts";
-        $stmt_reviews = $pdo->prepare($sql_reviews);
-        $stmt_reviews->execute();
-        $reviews = $stmt_reviews->fetchAll(PDO::FETCH_ASSOC);
-        if ($reviews) {
-            foreach ($reviews as $review) {
+            $stmt = $pdo->query("SELECT goods_id, category, image FROM goods");
+            $stmt->execute();
+            $goods = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($goods as $good) {
                 echo '<div class="category-card">';
-                echo    '<p>' . $review['category'] . '</p>';
-                echo    '<p><img src="'.$review['file_name'].'"></p>';
+                echo    '<a href="details.php?id=', $good['goods_id'], '">';
+                echo    '<img src="images/' . $good['image'] . '" alt="', $good['category'], '" width="150" height="150"></a>';
+                echo    $good['category'];
                 echo '</div>';
             }
-        }
-
-        
-    ?>
-</div>
+            ?>
+        </div>
+    </section>
 
 </body>
+
 </html>
