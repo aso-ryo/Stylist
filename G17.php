707a9880008bc67b1356d89b7a1005ae62b4d6ee
2301145 <2301145@s.asojuku.ac.jp>
2024-11-28 13:01:34 +0900

    1

--- G17.php
+++ G17.php
@@ -5,14 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/G17.css">
     <link rel="stylesheet" href="./css/reset.css">
-    <title>ログアウト画面
-    </title>
+    <title>ログアウト画面</title>
 </head>
 <body>
     <div class="a">
         <br><br><br>
         <h1>Stylista</h1><br>
-        <h3>ログアウトしました</h3><br>
+        <h3>ログアウトしました</h3>
         <h2>ログイン</h2><br>
         <p><input type="text" class="mail" name="mail" placeholder="メールアドレス"></p><br>
         <p><input type="text" class="pass" name="pass"　 placeholder="パスワード"></p><br><br>
