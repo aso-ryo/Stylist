dd3bbf92b5f161942c5f2383a4337b61f62b56a2
2301145 <2301145@s.asojuku.ac.jp>
2024-11-28 13:02:20 +0900

    1

--- G1.php
+++ G1.php
@@ -5,21 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン画面</title>
-    <link rel="stylesheet" href=".vscode/CSS/G1.css">
-    <link rel="stylesheet" href=".vscode/CSS/reset.css">
+    <link rel="stylesheet" href="./css/G17.css">
+    <link rel="stylesheet" href="./css/reset.css">
 </head>
 <body>
+<div class="a">
+    <br><br><br>
+    <h1>Stylista</h1><br>
+    <h3>ログイン</h3><br>
 
-<h1>Stylista</h1>
-<h3>ログイン</h3>
-
-<input class="mail" type="text" name="mail" placeholder="メールアドレス"><br>
-<input class="pass" name="pass" placeholder="パスワード"><br>
-<form action="G4.php" method="post">
-<button class="login-button" name="login">ログイン</button><br>
-</form>
+    <p><input class="mail" type="text" name="mail" placeholder="メールアドレス"></p><br>
+    <p><input class="pass" name="pass" placeholder="パスワード"></p><br><br>
+    <form action="G4.php" method="post">
+    <p><button class="login-button" name="login">ログイン</button><br><br>
 
 <form action="G2.php" method="post">
-<button class="new_user" name="touroku">新規会員登録はこちら</button>
+<button class="new" name="touroku">新規会員登録はこちら</button>
 </form>
-</body>　
+</div>
+</body>
