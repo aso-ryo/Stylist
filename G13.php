d9ac5c87050f5840e94ba9be3afdb77520348640
muuya-k <2301160@s.asojuku.ac.jp>
2024-11-28 09:56:18 +0900

    a

--- G13.php
+++ G13.php
@@ -12,9 +12,23 @@ Stylista
     <button type="submit">検索</button>
     <button type="submit" name="kato"></button>
     <button type="submit" name="favorite"></button>
-    <button type="submit" name="mypage"></button>
-
-    <p>お気に入り</p>
+    <button type="submit" name="mypage"></button>お気に入り
+    <?php
+        $pdo=new PDO('mysql:host=mysql309.phy.lolipop.lan;
+                    dbname=LAA1554862-kaihatsu;charset=utf8',
+                    'LAA1554862','aso2024');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                   
+        $stmt=$pdo->query("SELECT goods_id, category, image FROM goods");
+        $stmt->execute();
+        $goods=$stmt->fetchAll(PDO::FETCH_ASSOC);
+         
+        foreach ($goods as $good){
+            echo '<a href="details.php?id=',$good['goods_id'],'">';
+            echo '<img src="images/'.$good['image'].'" alt="',$good['category'],'" width="150" height="150"></a>';
+            echo $good['category'];
+        }
+        ?>  
     <a href="">すべて見る</a>
 
     <p>お知らせ</p>
--- G4.php
+++ G4.php
@@ -7,30 +7,17 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/reset.css">
-    <link rel="stylesheet" href="./css/G4.css">
-    <link rel="stylesheet" href="./css/header.css">
-    <title>トップページ</title>
+    <link rel="stylesheet" href=".vscode/CSS/G4.css">
+    <title>Document</title>
 </head>
 <body>
-<header>
-        <a href="" class="site__name">Stylista</a>
-        <form class="search__box" action="" method="post">
-            <input class="search__bar" type="text" name="query" placeholder="アイテムの検索">
-            <button type="submit"><i class="search__icon bi bi-search"></i>
-            </button>
-        </form>
-        <form action="" method="post">
-            <button type="submit"><i class="header__icon bi bi-cart"></i></button>
-        </form>
-        <form action="" method="post">
-            <button type="submit"><i class="header__icon bi bi-star"></i></button>
-        </form>
-        <form action="" method="post">
-            <button type="submit"><i class="header__icon bi bi-person"></i></button>
-        </form>
-    </header>
-
+    Stylista
+    <input type="text" name="query" placeholder="アイテムの検索">
+    <button type="submit">検索</button>
+    <button type="submit" name="kato"></button>
+    <button type="submit" name="favorite"></button>
+    <button type="submit" name="mypage"></button>
+    <br>
 
     
     <p>カテゴリーから探す</p>
@@ -55,24 +42,23 @@ session_start();
     <form action="" method="post">
     <button name=ladies type="submit"></button>
     </form>
-    <?
-$pdo=new PDO('mysql:host=mysql309.phy.lolipop.lan;
-dbname=LAA1554862-kaihatsu;charset=utf8',
-'LAA1554862',
-'aso2024');
-
-    $sql_reviews = "SELECT * FROM image_posts";
-        $stmt_reviews = $pdo->prepare($sql_reviews);
-        $stmt_reviews->execute();
-        $reviews = $stmt_reviews->fetchAll(PDO::FETCH_ASSOC);
-        if ($reviews) {
-            foreach ($reviews as $review) {
-                echo '<p>' . $review['category'] . '</p>';
-                echo '<p><img src="'.$review['file_name'].'"></p>';
-                echo '<hr>';
-            }
+    <?php
+        $pdo=new PDO('mysql:host=mysql309.phy.lolipop.lan;
+                    dbname=LAA1554862-kaihatsu;charset=utf8',
+                    'LAA1554862','aso2024');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                   
+        $stmt=$pdo->query("SELECT goods_id, category, image FROM goods");
+        $stmt->execute();
+        $goods=$stmt->fetchAll(PDO::FETCH_ASSOC);
+         
+        foreach ($goods as $good){
+            echo '<a href="details.php?id=',$good['goods_id'],'">';
+            echo '<img src="images/'.$good['image'].'" alt="',$good['category'],'" width="150" height="150"></a>';
+            echo $good['category'];
         }
-            ?>
+        ?>  
+    </div>
 
 </body>
 </html>
