e26141bdc48d22918ab0d32e3f9615ed2dc6f04e
muuya-k <2301160@s.asojuku.ac.jp>
2024-11-29 10:51:13 +0900

    a

--- G11.php
+++ G11.php
@@ -8,13 +8,18 @@
 </head>
 <body>
 Stylista
+<form action="G11.php" method="post">
     <input type="text" name="query" placeholder="アイテムの検索">
     <button type="submit">検索</button>
+</form>
+
     <button type="submit" name="kato"></button>
     <button type="submit" name="favorite"></button>
     <button type="submit" name="mypage"></button>
     <?php
-        echo $_POST['query'],'の検索結果';
+    // "query" の値を受け取る
+        $query = $_POST['query'] ?? ''; // デフォルト値は空文字
+        echo $query,'の検索結果';
         ?>
     <?php
         $pdo=new PDO('mysql:host=mysql309.phy.lolipop.lan;
@@ -23,7 +28,7 @@ Stylista
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                    
         $stmt=$pdo->prepare("SELECT goods_id, category, image FROM goods where `explain` LIKE :query");
-        $stmt->bindValue(':query', '%' . $_POST['query'] . '%', PDO::PARAM_STR);
+        $stmt->bindValue(':query', '%' . $query . '%', PDO::PARAM_STR);
         $stmt->execute();
         $goods=$stmt->fetchAll(PDO::FETCH_ASSOC);
          
