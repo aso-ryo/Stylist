664b600e3e4862f54c5a75994965369bfe28acbf
koudamiyuu <2301157@s.asojuku.ac.jp>
2024-12-02 02:17:09 +0900

    s

--- G8.php
+++ G8.php
@@ -15,19 +15,19 @@ session_start();
 </head>
 <body>
 <header>
-        <a href="" class="site__name">Stylista</a>
+        <a href="G4.php" class="site__name">Stylista</a>
         <form class="search__box" action="G11.php" method="post">
             <input class="search__bar" type="text" name="query" placeholder="アイテムの検索">
             <button type="submit"><i class="search__icon bi bi-search"></i>
             </button>
         </form>
-        <form action="" method="post">
+        <form action="G8.php" method="post">
             <button type="submit"><i class="header__icon bi bi-cart"></i></button>
         </form>
-        <form action="" method="post">
+        <form action="G12.php" method="post">
             <button type="submit"><i class="header__icon bi bi-star"></i></button>
         </form>
-        <form action="" method="post">
+        <form action="G13.php" method="post">
             <button type="submit"><i class="header__icon bi bi-person"></i></button>
         </form>
     </header>
