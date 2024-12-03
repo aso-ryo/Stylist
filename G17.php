ddd6d54d171822423094713caf9f6ecda2849929
koudamiyuu <2301157@s.asojuku.ac.jp>
2024-12-03 10:44:52 +0900

    s

--- G17.php
+++ G17.php
@@ -9,13 +9,13 @@ if (isset($_COOKIE['PHPSESSID'])) {
 
 session_destroy();
 
-echo '<form action="G1.php" method="post">';
-$message= 'ログアウトしました';
-echo '</form>';
 
-echo '<a href="tina10-2login_in.php">loginへ戻る</a>';
+$message= 'ログアウトしました';
+
+
+
+header('Location: G1.php?message=' . urlencode('ログアウトしました'));
+exit;
 ?>
 
-    <title>ログアウト画面</title>
-    <link rel="stylesheet" href=".vscode/CSS/reset.css">
-    <link rel="stylesheet" href=".vscode/CSS/G17.css">
+
